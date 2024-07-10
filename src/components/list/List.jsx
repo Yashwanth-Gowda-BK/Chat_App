c03f233da6576a40d8ee97d086e06b31fecc7069
Yashwanth B K <yashwanthgowdabk1@gmail.com>
2024-07-10 18:41:32 +0530

    user and list components

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,7 +1,12 @@
+import Chatlist from "./chatList/Chatlist";
 import "./list.css"
+import Userinfo from "./Userinfo/Userinfo";
 
 const List = () => {
-  return <div className="">List</div>;
+  return <div className="list">
+    <Userinfo/>
+    <Chatlist/>
+  </div>;
 };
 
 export default List;
---
+++ src/components/list/chatList/Chatlist.jsx
@@ -0,0 +1,7 @@
+import "./chatlist.css"
+
+const Chatlist = () => {
+  return <div className="chatlist">Chatlist</div>;
+};
+
+export default Chatlist;
