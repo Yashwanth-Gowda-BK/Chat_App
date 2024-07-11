36e1e1d319f09c4fd4a652bd5b664f467b535a62
Yashwanth B K <yashwanthgowdabk1@gmail.com>
2024-07-11 16:43:53 +0530

    updated detail component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
 
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
@@ -105,6 +111,7 @@ const Chat = () => {
             <span>1 min ago</span>
           </div>
         </div>
+        <div className="" ref={endRef}></div>
       </div>
       <div className="bottom">
         <div className="icons">
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -6,11 +6,8 @@ const Detail = () => {
       <div className="user">
         <img src="./avatar.png" alt="" />
         <h2>jane nkan</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consecte
-        </p>
+        <p>Lorem ipsum, dolor sit amet consecte</p>
       </div>
-
 
       <div className="info">
         <div className="option">
@@ -31,17 +28,13 @@ const Detail = () => {
             <img src="./arrowDown.png" alt="" />
           </div>
           <div className="photos">
-
-
             <div className="photoItem">
               <div className="photoDetail">
                 <img src="https://th.bing.com/th?id=ORMS.0f90748c9383f402b1f5b69173c65d3c&pid=Wdp&w=612&h=328&qlt=90&c=1&rs=1&dpr=1&p=0" />
                 <span>photo_2024</span>
               </div>
-            <img src="./download.png" className="icon" alt="" />
+              <img src="./download.png" className="icon" alt="" />
             </div>
-
-            
           </div>
         </div>
         <div className="option">
@@ -51,6 +44,7 @@ const Detail = () => {
           </div>
         </div>
         <button>Block User</button>
+        <button className="logout">Log Out</button>
       </div>
     </div>
   );
