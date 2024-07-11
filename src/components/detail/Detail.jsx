fcde72b2b96504268cd8ce0b65710b180a4a2af7
Yashwanth B K <yashwanthgowdabk1@gmail.com>
2024-07-11 10:45:36 +0530

    detail component design

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,59 @@
 import "./detail.css";
 
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>jane nkan</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consecte
+        </p>
+      </div>
+
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span> Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://th.bing.com/th?id=ORMS.0f90748c9383f402b1f5b69173c65d3c&pid=Wdp&w=612&h=328&qlt=90&c=1&rs=1&dpr=1&p=0" />
+                <span>photo_2024</span>
+              </div>
+            <img src="./download.png" className="icon" alt="" />
+            </div>
+
+            
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
