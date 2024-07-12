5a466f3df18092a1691ed3ff585dc3f4b9b0ed20
Yashwanth B K <yashwanthgowdabk1@gmail.com>
2024-07-12 12:11:46 +0530

    complete log in and log out. fetching data using zustand state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./components/login/userStore";
 
 const App = () => {
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  console.log(currentUser);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
 
   return (
     <div className="container">
-      {user ? (
+      {currentUser ? (
         <>
           <List />
           <Chat />
@@ -18,7 +36,7 @@ const App = () => {
       ) : (
         <Login />
       )}
-      <Notification/>
+      <Notification />
     </div>
   );
 };
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,3 +1,4 @@
+import { auth } from "../../lib/firebase";
 import "./detail.css";
 
 const Detail = () => {
@@ -44,7 +45,7 @@ const Detail = () => {
           </div>
         </div>
         <button>Block User</button>
-        <button className="logout">Log Out</button>
+        <button className="logout" onClick={()=>auth.signOut()}>Log Out</button>
       </div>
     </div>
   );
