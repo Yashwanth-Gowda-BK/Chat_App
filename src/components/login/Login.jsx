b46d316f3e839a01759a723c6612f51bd61cb7ef
Yashwanth B K <yashwanthgowdabk1@gmail.com>
2024-07-12 11:10:36 +0530

    sign with email and password

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth, db } from "../../lib/firebase.js";
 import { doc, setDoc } from "firebase/firestore";
 import upload from "../../lib/upload.js";
@@ -23,8 +26,21 @@ const Login = () => {
     }
   };
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
+    
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
   const handleRegister = async (e) => {
     e.preventDefault();
