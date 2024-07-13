6e24e2baa6914db48f7664c79b95e9570a84b03d
Yashwanth B K <yashwanthgowdabk1@gmail.com>
2024-07-13 20:54:39 +0530

    handling sender and receiver message and handling image

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -6,11 +6,16 @@ import { doc } from "firebase/firestore";
 import { db } from "../../lib/firebase";
 import { useChatStore } from "../../lib/chatStore";
 import { useUserStore } from "../../lib/userStore";
+import upload from "../../lib/upload";
 
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [chat, setChat] = useState();
   const [text, setText] = useState("");
+  const [img, setImg] = useState({
+    file: null,
+    url: "",
+  });
 
   const { chatId, user } = useChatStore();
   const { currentUser } = useUserStore();
@@ -36,14 +41,29 @@ const Chat = () => {
     setOpen(false);
   };
 
+  const handelImg = (e) => {
+    if (e.target.files[0]) {
+      setImg({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
+
   const handleSend = async () => {
     if (text === "") return;
+
+    let imgUrl = null;
     try {
+      if (img.file) {
+        imgUrl = await upload(img.file);
+      }
       await updateDoc(doc(db, "chats", chatId), {
         messages: arrayUnion({
           senderId: currentUser.id,
           text,
           createdAt: new Date(),
+          ...(imgUrl && { img: imgUrl }),
         }),
       });
 
@@ -73,6 +93,13 @@ const Chat = () => {
     } catch (error) {
       console.log(error);
     }
+
+    setImg({
+      file: null,
+      url: "",
+    });
+
+    setText("");
   };
 
   return (
@@ -96,7 +123,7 @@ const Chat = () => {
       </div>
       <div className="center">
         {chat?.messages?.map((message) => (
-          <div className="message own" key={message?.createAt}>
+          <div className={message.senderId === currentUser?.id ?"message own": "message"} key={message?.createAt}>
             <img src="./avatar.png" alt="" />
             <div className="texts">
               {message.img && <img src={message.img} alt="" />}
@@ -105,12 +132,25 @@ const Chat = () => {
             </div>
           </div>
         ))}
-
+        {img.url && <div className="message own">
+          <div className="texts">
+            <img src={img.url} alt="" />
+          </div>
+        </div>}
         <div className="" ref={endRef}></div>
       </div>
       <div className="bottom">
         <div className="icons">
-          <img src="./img.png" alt="" />
+          <label htmlFor="file">
+            <img src="./img.png" alt="" />
+          </label>
+          <input
+            type="file"
+            name=""
+            id="file"
+            style={{ display: "none" }}
+            onChange={handelImg}
+          />
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
